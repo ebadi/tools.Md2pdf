b5434a0147f75fc78fb3d6545d27590d276c1169
Hamid Ebadi <hamid.ebadi@gmail.com>
2020-11-11 13:00:13 +0100

    increase memory size

--- markdown_to_pdf/markdown_to_pdf.py
+++ markdown_to_pdf/markdown_to_pdf.py
@@ -369,7 +369,7 @@ def generate_default_cover_file(input_conf_file, output_file):
 
 
 def convert_from_GFM(original_md, converted_md):
-     call(["pandoc", "+RTS", "-K1000000000", "-RTS", --from", "markdown_github", "--to", "markdown", "--output", converted_md, original_md])
+     call(["pandoc", "+RTS", "-K1000000000", "-RTS", "--from", "markdown_github", "--to", "markdown", "--output", converted_md, original_md])
 
 def render_pdf_cover(input_md_path, output_pdf_path):
     """ convert a MD cover to a PDF """
